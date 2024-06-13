9fad91434ea909fedca9bceeee4701b2110a56fa
shashwatthapa <thapashashwat@gmail.com>
2024-06-13 10:47:36 +0545

    scrollable

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,27 +1,68 @@
 import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
+  Profile({super.key});
+  final scrollController = ScrollController();
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(Icons.arrow_back),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back),
+        ),
+        title: Text(
+          "Profile",
+          style: TextStyle(
+            color: Colors.white,
           ),
-          title: Text(
-            "Profile",
-            style: TextStyle(
-              color: Colors.white,
+        ),
+        backgroundColor: Colors.green,
+      ),
+      body: DecoratedBox(
+        decoration: BoxDecoration(color: Colors.purple),
+        child: RawScrollbar(
+          controller: scrollController,
+          trackVisibility: true,
+          thumbVisibility: true,
+          thumbColor: Colors.green,
+          trackColor: Colors.blue,
+          child: SingleChildScrollView(
+            controller: scrollController,
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment:
+                  MainAxisAlignment.spaceEvenly, //mainaxialignment --> Y-axis
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment:
+                  CrossAxisAlignment.center, //crossaxisalignmnet --> X-axis
+              children: [
+                Container(
+                  height: 250,
+                  width: 150,
+                  color: Colors.orange,
+                ),
+                Container(
+                  height: 400,
+                  width: 400,
+                  color: Colors.black,
+                  child: Stack(
+                    children:[]
+                  )
+                ),
+                Container(
+                  height: 250,
+                  width: 100,
+                  color: Colors.blue,
+                ),
+              ],
             ),
           ),
-          backgroundColor: Colors.green,
         ),
-        body: Center(
-          child: Text("Profile"),
-        ));
+      ),
+    );
   }
 }
